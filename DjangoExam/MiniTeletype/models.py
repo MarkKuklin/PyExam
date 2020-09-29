e496f82c5dee2971ac9aec760fc51640d49f0b7d
unknown <Almat@N205-1.local.itstep>
2020-09-29 21:18:54 +0600

    2909 inaud

--- DjangoExam/MiniTeletype/models.py
+++ DjangoExam/MiniTeletype/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    avatar = models.ImageField()
+
+# class Post(models.Model):
+#     header = models.CharField(max_length=256)
+
