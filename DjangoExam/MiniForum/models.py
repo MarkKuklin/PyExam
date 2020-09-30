77947a9aa089b087b93d9c26bf2a70ffe83ac745
Mark ItStep <you@example.com>
2020-09-30 21:11:04 +0600

    nnn

--- DjangoExam/MiniForum/models.py
+++ DjangoExam/MiniForum/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
+# class User(models.Model):
+#     username = models.CharField(max_length=50)
+#     email = models.EmailField()
+#     password = models.CharField(max_length=50)
+
+#     def self(self):
+#         return self.username
 
 class Theme(models.Model):
     name = models.CharField(max_length=50)
